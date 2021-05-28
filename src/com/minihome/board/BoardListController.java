01123c3a976cf8c1f1f3f1ffb4549fc29f532214
Chan <Chan@211.192.31.121>
2021-05-28 19:51:55 +0900

    a

--- src/com/minihome/board/BoardListController.java
+++ src/com/minihome/board/BoardListController.java
@@ -1,5 +1,17 @@
 package com.minihome.board;
 
-public class BoardListController {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("board.list.do")
+public class BoardListController extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	}
 
 }
