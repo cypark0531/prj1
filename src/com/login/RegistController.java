8c455acb05ce829dc1b25956e8b1fee504ac966b
jhta <jhta@DESKTOP-IK98S44>
2021-05-28 17:51:23 +0900

    1.0.1

--- src/com/login/RegistController.java
+++ src/com/login/RegistController.java
@@ -13,4 +13,14 @@ public class RegistController extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.sendRedirect(req.getContextPath()+"/login/regist.jsp");
 	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String id=req.getParameter("id");
+		String pwd=req.getParameter("pwd");
+		String name=req.getParameter("name");
+		String email=req.getParameter("email");
+		String phone=req.getParameter("phone");
+		String pwdask=req.getParameter("pwdask");
+		String pwdans=req.getParameter("pwdans");
+	}
 }
