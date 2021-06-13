30b91a3f8aa070e35723869f62b7c69d00820fb1
Chan <Chan@14.39.24.87>
2021-06-13 23:56:46 +0900

    asdf

---
+++ src/com/minihome/charge/chargeInsertController.java
@@ -0,0 +1,32 @@
+package com.minihome.charge;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.minihome.dao.MembersDao;
+@WebServlet("/charge/insert")
+public class chargeInsertController extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String id = (String)req.getSession().getAttribute("id");
+		String gid = (String)req.getSession().getAttribute("gid");
+		String scash = req.getParameter("cash");
+		int cash = 0;
+		if(scash.equals("another")) {
+			cash =  Integer.parseInt(req.getParameter("another"))/100;
+		}else {
+			cash = Integer.parseInt("cash");
+		}
+		int n = MembersDao.getIntstance().moneyRefund(id, cash);
+		if(n>0) {
+		}else {
+			System.out.println("실패");
+		}
+		resp.sendRedirect(req.getContextPath()+"/home");
+	}
+}
